dc013108700b545009a6fba65aa2768c70323359
slice of code <slice_of_code@mail.ru>
2024-02-29 19:32:51 +0400

    input

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,5 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
-import {Button} from "@mui/material";
+import {Button, TextField} from "@mui/material";
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
@@ -31,7 +31,7 @@ export function AddItemForm(props: AddItemFormPropsType) {
     }
 
     return <div>
-        <input value={title}
+        <TextField value={title}
                onChange={onChangeHandler}
                onKeyPress={onKeyPressHandler}
                className={error ? "error" : ""}
